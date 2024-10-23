3c4c1608ba40f35686aacb49a3f74a89b06df94f
JuanMartinMena <juanmartinmenac@gmail.com>
2024-10-23 13:25:08 +0000

    Add clases for user/amenities/places/reviews

---
+++ app/models/place.py
@@ -0,0 +1,20 @@
+from app.models import BaseModel
+from app.models.user import User
+
+class Place(BaseModel):
+    def __init__(self, title, description, price, latitude, longitude, owner):
+        super().__init__()
+        self.title = title
+        self.description = description
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
+        self.owner = owner  # Se debe validar que el propietario sea un User
+        self.reviews = []
+        self.amenities = []
+
+    def add_review(self, review):
+        self.reviews.append(review)
+
+    def add_amenity(self, amenity):
+        self.amenities.append(amenity)
---
+++ app/models/review.py
@@ -0,0 +1,11 @@
+from app.models import BaseModel
+from app.models.place import Place
+from app.models.user import User
+
+class Review(BaseModel):
+    def __init__(self, text, rating, place, user):
+        super().__init__()
+        self.text = text
+        self.rating = rating
+        self.place = place  # Validar que el lugar exista
+        self.user = user  # Validar que el usuario exista
