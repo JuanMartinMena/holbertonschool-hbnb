83bcaaee0bc0020f7dbf0406618f7813516fe3e9
ariel <ariel@gmail.com>
2024-11-13 10:48:28 -0300

    1- Bcrypt incompleto

--- Parte3/app/__init__.py
+++ Parte3/app/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from flask_bcrypt import Bcrypt
 
+bcrypt = Bcrypt()
 def create_app(config_class="config.DevelopmentConfig"):
+    
     app = Flask(__name__)
     app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
     # Registrar el namespace de usuarios
     api.add_namespace(users_ns, path='/api/v1/users')
-    
+    bcrypt.init_app(app)
     return app
--- Parte3/app/models/user.py
+++ Parte3/app/models/user.py
@@ -1,5 +1,6 @@
 from app.models.base_for_all import BaseModel
 
+
 class User(BaseModel):
     def __init__(self, first_name, last_name, email, is_admin=False):
         super().__init__()
@@ -7,3 +8,10 @@ class User(BaseModel):
         self.last_name = last_name
         self.email = email
         self.is_admin = is_admin
+
+    def hash_password(self, password):
+        self.hash_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+
+    def verify_password(self, password):
+        return bcrypt.check_password_hash(self.password, password)
