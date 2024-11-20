293b2df8894d107455aaa90f9380cddba381de00
JuanMartinMena <juanmartinmenac@gmail.com>
2024-11-20 17:59:33 +0000

    trying to fix amenitites problem

--- Parte2/app/models/amenity.py
+++ Parte2/app/models/amenity.py
@@ -4,3 +4,12 @@ class Amenity(BaseModel):
     def __init__(self, name):
         super().__init__()
         self.name = name
+
+    def to_dict(self):
+        """Convierte el objeto Amenity a un diccionario serializable"""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'created_at': self.created_at.isoformat(),  # Asegúrate de que 'created_at' sea un objeto datetime
+            'updated_at': self.updated_at.isoformat()   # Similar para 'updated_at'
+        }
